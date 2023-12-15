a01051e64da77b63ec54a1e5aab4f6119291ed8c
Sage Russell <5957505+Sageth@users.noreply.github.com>
2023-12-15 13:22:34 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,17 @@
+Specs to build Nginx RPM with Brotli module
+==============================================
+
+Building the RPM
+----------------
+
+Run 
+```markdown
+./buildnginx.sh
+```
+
+Updating to Newer Version of Nginx
+-----------------------------------------------
+
+Update nginx Version in the SPECS/nginx.spec file.
+nginx: http://nginx.org/en/download.html
+
---
+++ buildnginx.sh
@@ -0,0 +1,14 @@
+# Re-prepare the folders
+rm -f  ./SOURCES/*.gz && \
+rm -f  ./SOURCES/*.zip && \
+rm -rf ./BUILD/ngx_brotli/ && \
+rm -rf ./ngx_brotli/ && \
+rm -rf ./BUILD/* && \
+rm -rf ./BUILDROOT/* && \
+
+git clone https://github.com/google/ngx_brotli.git ./BUILD/ngx_brotli/ && \
+
+NX_VERSION=`grep "Version" ./SPECS/nginx.spec | cut -d " " -f 2` && \
+wget --no-verbose http://nginx.org/download/nginx-${NX_VERSION}.tar.gz -P ./SOURCES/ && \
+
+rpmbuild --define "_sourcedir $(pwd)/SOURCES" --define "_builddir $(pwd)/BUILD" -bb $(pwd)/SPECS/nginx.spec
